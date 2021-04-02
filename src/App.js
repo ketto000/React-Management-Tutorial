3e56bfa5d554e4a982689b1ee1d169c98c4dcd52
neo <ketto@nate.com>
2021-04-02 16:03:22 +0900

    222

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="gray-background">
         <img src={logo} let="logo" />
         <h2>Rain Forest Alliance Certified1ddddddssss2222s1</h2>
+        <H2>i'm  for the first time use_git_hub </H2>
     </div>
   );
 }
