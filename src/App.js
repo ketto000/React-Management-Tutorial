414f96fcd1db7d511c80fc4702cedfb631a8c464
neo <ketto@nate.com>
2021-04-02 15:50:27 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+        <img src={logo} let="logo" />
+        <h2>Rain Forest Alliance Certified1sss1</h2>
+    </div>
+  );
+}
+
+export default App;
