fcd552788e39f899337e6719b271b68a1eefd70d
neo <ketto@nate.com>
2021-04-02 16:02:23 +0900

    dddddd2222d2222

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+        <img src={logo} let="logo" />
+        <h2>Rain Forest Alliance Certified1ddddddssss2222s1</h2>
+    </div>
+  );
+}
+
+export default App;
