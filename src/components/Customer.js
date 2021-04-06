4e9ef7e01bb5189f9bc97f31f318494d64adc678
neo <ketto@nate.com>
2021-04-06 10:55:13 +0900

    5장

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -4,8 +4,10 @@ class Customer extends React.Component{
     render(){
       return(
           <div>
+              <div class='profiles'>
             <CustomreProfile image={this.props.image} name={this.props.name} id={this.props.id}/>
             <CustomerInfo           name={this.props.name}          birthday={this.props.birthday}          gender={this.props.gender}          jop={this.props.jop}          />
+              </div>
           </div>
       );
     }
@@ -15,7 +17,7 @@ class CustomreProfile extends React.Component{
     render(){
         return(
             <div>
-                <img src={this.props.image} alt="profile" width="150" />
+                <img src={this.props.image} alt="profile" width="150" height="200"/>
                 <h2>{this.props.name}({this.props.id})</h2>
             </div>
         );
