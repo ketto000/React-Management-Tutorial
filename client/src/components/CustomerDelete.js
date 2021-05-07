dbc242a85c492b39e4c638cf69506e6ae7822ce1
neo <ketto@nate.com>
2021-05-07 18:33:50 +0900

    15강 -AddCustomer DELETE soft

--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -9,14 +9,61 @@ class customerDelete extends Component{
             id: id
         });
         this.props.stateRefresh();
-
     }
+
+
+    deleteCustomerRS(id){
+        const url ='./api/customers/rs/'+id;
+        fetch(url,{
+            method:'delete',
+            id: id
+        });
+        this.props.stateRefresh();
+    }
+
+    useConfirm = (message = null, onConfirm, onCancel) => {
+        if (!onConfirm || typeof onConfirm !== "function") {  return;   }
+        if (onCancel && typeof onCancel !== "function") {  return;   }
+        const confirmAction = () => {
+            if (window.confirm(message)) {
+                onConfirm();
+            } else {
+                onCancel();
+            }
+        };
+        return confirmAction;
+    };
+
+    deleteConfirm = () => {
+        this.deleteCustomer(this.props.id)
+        console.log("삭제했습니다.");
+    };
+
+    deleteConfirmRS = () => {
+        this.deleteCustomerRS(this.props.id)
+        console.log("삭제했습니다.");
+    };
+
+    cancelConfirm = () => console.log("취소했습니다.");
+
+    confirmDelete = this.useConfirm(
+        "소프트삭제하시겠습니까?",
+        this.deleteConfirm,
+        this.cancelConfirm
+    );
+
+    confirmDeleteRS = this.useConfirm(
+        "삭제하시겠습니까?",
+        this.deleteConfirmRS,
+        this.cancelConfirm
+    );
 
     render(){
         return(
-            <button onClick={(e) => { this.deleteCustomer(this.props.id)}}>삭제</button>
+            <div><button onClick={(e) => {this.confirmDelete() }} >소프트 삭제</button>
+                <button onClick={(e) => { this.confirmDeleteRS()}}>삭제</button></div>
         )
     }
-
 }
+
 export default customerDelete;
